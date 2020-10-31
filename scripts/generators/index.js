f8803151c305f45b6ec9d5f364b48aa196e25f3e
rubilmax <rmilon@gmail.com>
2020-10-31 11:29:49 +0100

    Fixed generator

--- scripts/generators/index.js
+++ scripts/generators/index.js
@@ -1,13 +1,11 @@
 const componentGenerator = require('./component/index');
 const moduleGenerator = require('./module/index');
 const actionGenerator = require('./action/index');
-const pageGenerator = require('./page/index');
 const serviceGenerator = require('./service/index');
 
 module.exports = (plop) => {
   plop.setGenerator('Component', componentGenerator);
   plop.setGenerator('Module', moduleGenerator);
   plop.setGenerator('Action', actionGenerator);
-  plop.setGenerator('Page', pageGenerator);
   plop.setGenerator('Service', serviceGenerator);
 };
